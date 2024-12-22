1952925976add596e02bea536c4cb3c6a9a8ac93
Vitalya <vitalikt_22@mail.ru>
2024-12-22 18:59:48 +0400

    Adding Request and RequestsTable pages + page Login markup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
-import { Test, EditTest, Main } from './pages/index'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { saveQuestionsAsync } from './actions/index'
 
 const AppColumn = styled.div`
 	display: flex;
@@ -19,31 +15,22 @@ const Page = styled.div`
 `
 
 const App = () => {
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		dispatch(saveQuestionsAsync())
-	}, [dispatch])
-
 	return (
 		<AppColumn>
 			<Page>
 				<Routes>
 					<Route
-						path='/'
-						element={<Main />}
+						path='/request'
+						element={<Request />}
 					/>
 					<Route
-						path='/test'
-						element={<Test />}
+						path='/requests'
+						element={<RequestsTable />}
 					/>
 					<Route
-						path='/editTest'
-						element={<EditTest />}
+						path='/login'
+						element={<Login />}
 					/>
-					{/* <Route
-						path='*'
-						element={<Error error={} />}/> */}
 				</Routes>
 			</Page>
 		</AppColumn>
